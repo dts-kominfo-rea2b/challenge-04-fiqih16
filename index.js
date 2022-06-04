29756b3d386da248501060977ee1b3da12ae2e70
fiqih16 <fiqih1666@gmail.com>
2022-06-04 21:36:26 +0700

    New

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 // ! JANGAN DIMODIFIKASI
 const dates = [
-  "2019-01-02", // 1546387200 (epoch time, dalam detik)
-  "2022-05-06 00:00:00 UTC-2", // 1651802400 (epoch time, dalam detik)
-  "2021-03-04 07:00:00 UTC", // 1614841200 (epoch time, dalam detik)
-  "2021-04-05 00:00:00 UTC+2", // 1617573600 (epoch time, dalam detik)
-  "2020-02-03 UTC+7", // 1580662800 (epoch time, dalam detik)
+  '2019-01-02', // 1546387200 (epoch time, dalam detik)
+  '2022-05-06 00:00:00 UTC-2', // 1651802400 (epoch time, dalam detik)
+  '2021-03-04 07:00:00 UTC', // 1614841200 (epoch time, dalam detik)
+  '2021-04-05 00:00:00 UTC+2', // 1617573600 (epoch time, dalam detik)
+  '2020-02-03 UTC+7', // 1580662800 (epoch time, dalam detik)
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+// const createDate = null;
+const createDate = (newData, index) => {
+  const sortDate = [];
+  for (let i = 0; i < newData.length; i++) {
+    const result = (new Date(newData[i]).getTime() / 1000).toString();
+    sortDate.push(result);
+  }
+  if (index === undefined) {
+    sortDate.sort((a, b) => a - b);
+    return sortDate.join(' - ');
+  } else {
+    sortDate.sort((a, b) => a + b);
+    return sortDate[index];
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
